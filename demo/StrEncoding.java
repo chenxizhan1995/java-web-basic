ad8704ec21c91d6f0603b08b449754fd30bf696d
chenxizhan <chenxizhan1995@163.com>
2021-05-01 09:36:56 +0800

    Java查看系统编码

--- demo/StrEncoding.java
+++ demo/StrEncoding.java
@@ -1,7 +1,16 @@
+import java.nio.charset.Charset;
+import java.util.logging.Logger;
+
 public class StrEncoding{
 
     public static void main(String[] args){
+        // 查看系统编码
+        System.out.println(System.getProperty("file.encoding"));
+        System.out.println(Charset.defaultCharset());
+
         String s1 = "hello,中国";
         System.out.println(s1);
+        Logger log = Logger.getLogger(StrEncoding.class.getName());
+        log.info(s1);
     }
 }
